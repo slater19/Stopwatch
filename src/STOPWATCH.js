5627f7af3a6ed79ad1496e97c23718a2f9369ea4
slater19 <sachinaggarwal19@gmail.com>
2024-10-13 11:59:51 +0530

    adding files

---
+++ src/STOPWATCH.js
@@ -0,0 +1,50 @@
+import React, { useState, useEffect } from "react";
+
+const Stopwatch = () => {
+  
+  const [time, setTime] = useState(0);
+   const [isRunning, setIsRunning] = useState(false)
+  
+  const  formatTime= (seconds) => {
+    const minutes = Math.floor(seconds/ 60);
+    const  remSeconds = seconds% 60;
+    return `${minutes}:${remSeconds<10?"0":""}${remSeconds}`;
+  };
+
+  const handleStart = () => {
+  
+    setIsRunning(!isRunning);
+    
+  }
+
+  const handleReset  = () => {
+  setIsRunning(false);
+  setTime(0);
+  }
+ 
+  useEffect(() => {
+    let intervalId;
+    if (isRunning) {
+      // setting time from 0 to 1 every 10 milisecond using javascript setInterval method
+      intervalId = setInterval(() => setTime(prev=>prev+1), 1000);
+    }
+    else{clearInterval(intervalId)}
+    return () => clearInterval(intervalId);
+  }, [isRunning]);
+
+  return (
+    <>
+        <h1>Stopwatch</h1>
+        <p>Time:{formatTime(time)}</p>
+        <button  onClick={handleStart}>
+          {isRunning ? "Stop" : "Start"}
+        </button>
+        <button  onClick={handleReset}>
+        reset
+        </button>
+      
+        </>
+  );
+};
+
+export default Stopwatch;
